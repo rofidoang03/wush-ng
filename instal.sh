efbc9d34259274b9ac0330d07116d9d628395f19
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-18 23:06:15 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -4,6 +4,9 @@
 read -p "Apakah Anda ingin menginstal wish (Y/n): " n
 if [[ "${n}" == "y" ]] || [[ "${n}" == "Y" ]]; then
         echo "[*] Menginstal wush-ng..."
+        sleep 3
+        echo "[*] Memperbarui daftar paket dari repositori..."
+        sleep 3
         depedensi(
         "aircrack-ng"
         "xterm"
@@ -12,8 +15,10 @@ if [[ "${n}" == "y" ]] || [[ "${n}" == "Y" ]]; then
         sleep 3
         for d in "${depedensi[@]}"; do
                 echo "[*] Menginstal ${d}..."
+                sleep 3
                 apt-get install "${d}"
-                echo "[+] Berhasil diinstal."
+                echo "[+] ${d} berhasil diinstal."
+                sleep 3
         done
         echo "[+] Semua depedensi yang diperlukan berhasil diinstal."
         lokasi_wush_ng="src/wush-ng"
