ba4cdfc0ff98ab14d2411680d7cc9adb2b8ecbc8
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 10:00:29 +0700

    Update sound.py

--- scripts/sound.py
+++ scripts/sound.py
@@ -1,10 +1,8 @@
 from gtts import gTTS
 import simpleaudio as sa
 import os
-import time
 
-text = """
-PERINGATAN
+text="""PERINGATAN
 
 Penyalahgunaan atau penggunaan program ini untuk
 aktivitas ilegal dilarang keras. Program ini
@@ -20,31 +18,24 @@ pembuat program.
 
 DENGAN MENGGUNAKAN PROGRAM INI, ANDA MENYATAKAN
 BAHWA ANDA MEMAHAMI DAN MENERIMA SYARAT DAN KETENTUAN
-DIATAS.
-"""
+DIATAS."""
 
-lang = "id"
+lang="id"
 
-# Memisahkan teks menjadi token (kata-kata)
-words = text.split()
+print("[*] Dengarkan Saya...")
 
-for word in words:
-    # Membuat suara untuk setiap kata
-    tts = gTTS(text=word, lang=lang)
-    tts.save("output.mp3")
+# Membuat suara dari teks
+tts = gTTS(text=text, lang=lang)
+tts.save("output.mp3")
 
-    # Menggunakan ffmpeg untuk mengonversi file MP3 ke WAV
-    os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
+# Menggunakan ffmpeg untuk mengonversi file MP3 ke WAV
+os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
 
-    # Memuat file WAV menggunakan simpleaudio
-    wave_obj = sa.WaveObject.from_wave_file("output.wav")
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
+# Memuat file WAV menggunakan simpleaudio
+wave_obj = sa.WaveObject.from_wave_file("output.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
 
-    # Menghapus file setelah selesai diputar
-    os.remove("output.mp3")
-    os.remove("output.wav")
-
-    # Memberi jeda sebentar sebelum melanjutkan ke kata berikutnya
-    time.sleep(0.5)
-  
+# Menghapus file setelah selesai diputar
+os.remove("output.mp3")
+os.remove("output.wav")
